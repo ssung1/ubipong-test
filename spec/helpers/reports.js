30789eba93d2375689b8a1986fed505f7b58f3d6
Shawfe Sung <shawfe@techie.com>
2020-12-23 19:02:15 -0500

    Added terminal reporter

--- spec/helpers/reports.js
+++ spec/helpers/reports.js
@@ -1,7 +1,14 @@
 const jasmineReporters = require('jasmine-reporters')
 
 const junitReporter = new jasmineReporters.JUnitXmlReporter({
-    savePath: 'reports',
-    consolidateAll: false
+  savePath: 'reports',
+  consolidateAll: false
+})
+
+const terminalReporter = new jasmineReporters.TerminalReporter({
+  verbosity: 3,
+  color: true,
+  showStack: true,
 })
 jasmine.getEnv().addReporter(junitReporter)
+jasmine.getEnv().addReporter(terminalReporter)
