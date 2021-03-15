c60b01d241d2094e4cd603a311086ce363ba5c91
Shawfe Sung <shawfe@techie.com>
2021-03-14 20:23:06 -0400

    Added notes about setting challonge API key

--- spec/config/environment.js
+++ spec/config/environment.js
@@ -1,7 +1,9 @@
 const environment = {
   apiHost: 'http://localhost:8080',
   challongeHost: 'https://api.challonge.com',
-  challongeApiKey: 'this is the api key from challonge',
+  // if using jasmine explorer, set jasmineExplorer.env in settings.json
+  // if using standard launch, set env to include api-key and set console to intergratedTerminal
+  challongeApiKey: process.env.CHALLONGE_API_KEY,
 }
 
 module.exports = environment
